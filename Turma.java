634b83add42ce6c01c2371104c58af04add2262c
granato-vitin <victor.granato.oliveira@escola.pr.gov.br>
2026-06-24 09:20:07 -0300

    Implement Turma class with student management

--- Turma.java
+++ Turma.java
@@ -1,5 +1,27 @@
 package br.com.escola.model;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Turma {
-
+  private String nome;
+  private List<Aluno> alunos;
+  
+  public Turma(String nome) {
+	  this.nome = nome;
+	  this.alunos = new ArrayList<>();
+  }
+  public String getNome() {
+	  return nome;
+  }
+  public List<Aluno>getAlunos(){
+	  return alunos;
+  }
+  public void adicionarAluno(Aluno aluno) {
+	  alunos.add(aluno);
+  }
+  @Override
+  public String toString() {
+	  return nome + "("+ alunos.size() + "alunos)";
+  }
 }
+

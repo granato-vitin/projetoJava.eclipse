3f46ed601f2253aae2446a2075aba6080f85c2a1
granato-vitin <victor.granato.oliveira@escola.pr.gov.br>
2026-06-24 09:19:12 -0300

    Add methods for managing Turma in EscolaService
    Implemented methods to manage 'Turma' objects including adding, listing, and searching.

--- EscolaService.java
+++ EscolaService.java
@@ -1,5 +1,23 @@
 package br.com.escola.service;
+import java.util.ArrayList;
+import java.util.List;
+import br.com.escola.model.Turma;
 
 public class EscolaService {
-
+	private List<Turma> turmas = new ArrayList<>();
+    public void adicionarTurma(Turma turma) {
+        turmas.add(turma);
+    }
+public List<Turma> listarTurmas(){
+    return turmas;
+}
+public Turma buscarTurma(int indice) {
+    if (indice >= 0 && indice < turmas.size()) {
+    	return turmas.get(indice);
+    }
+    return null;
+}
+public boolean estaVazia() {
+    return turmas.isEmpty();
+}
 }
